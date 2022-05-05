4b5cb1d9c11a474c332610a80b150756b88b1d09
Peter Molnar <petermolnar.hu@gmail.com>
2022-05-05 21:39:14 +0200

    Saving and reading files from the document directory.

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,10 +7,44 @@
 
 import SwiftUI
 
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+    
+    static func < (lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+    
+    
+}
+
+
 struct ContentView: View {
+    @State private var nr = 0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       Text("Hello World")
+            .onTapGesture {
+                let str = "Test message \(nr)"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+                nr += 1
+            }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        // find all possible document directories for this user.
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return paths[0]
     }
 }
 
