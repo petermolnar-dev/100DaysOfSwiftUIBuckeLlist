55043469106dcec2ebb54d73cf44e44e51ac3488
Peter Molnar <petermolnar.hu@gmail.com>
2022-05-12 12:08:38 +0200

    Day 70: Making the code neater, adding the coordinate to the `Location` struct.

--- BucketList/Location.swift
+++ BucketList/Location.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import MapKit
 
 struct Location: Identifiable, Codable, Equatable {
     let id: UUID
@@ -13,4 +14,13 @@ struct Location: Identifiable, Codable, Equatable {
     var description: String
     let latitude: Double
     let longitude: Double
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Where Queen Elisabeth lives with her dogs.", latitude: 51.501, longitude: -0.141)
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
