dbbf7d7be2732e6831c067dae9ea85f6ac010b57
Peter Molnar <petermolnar.hu@gmail.com>
2022-05-12 21:05:22 +0200

    Finishing Day 70, with the possibility to change the location name.

--- BucketList/UserLocationView.swift
+++ BucketList/UserLocationView.swift
@@ -12,6 +12,7 @@ struct UserLocationView: View {
     
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
     @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
     
     var body: some View {
         ZStack {
@@ -25,7 +26,12 @@ struct UserLocationView: View {
                         .background(.white)
                         .clipShape(Circle())
                     Text(location.name)
+                        // SwifUI Bug to resize the Text size.
+                            .fixedSize()
                 }
+                    .onTapGesture {
+                        selectedPlace = location
+                    }
                     
                 }
             }
@@ -50,6 +56,13 @@ struct UserLocationView: View {
                     .font(.title)
                     .clipShape(Circle())
                     .padding(.trailing)
+                }
+            }
+        }
+        .sheet(item: $selectedPlace) { place in
+            EditView(location: place) { newLocation in
+                if let index = locations.firstIndex(of: place) {
+                    locations[index] = newLocation
                 }
             }
         }
